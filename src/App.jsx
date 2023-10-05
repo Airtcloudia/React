ca8cb42f3a2e63da94fbdb6b548fca725ca5b620
Airtcloudia <klau26@hotmail.it>
2023-10-05 12:08:56 +0200

    add esercizio-14

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import hello from "./Hello"
+import Messaggio from "./Messaggio"
 
 function App() {
 
   return (
     <>
-     <Hello/>
-     <Hello/>
+     <Messaggio/>
+    
     </>
   )
 }
 
 export default App
 
-/*è possibile riutilizzare un componente più volte*/
