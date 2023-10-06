998e53cd2aae830ae70bc30add29ead290b45f10
Airtcloudia <klau26@hotmail.it>
2023-10-06 19:38:50 +0200

    add esercizio-25

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -1,11 +1,10 @@
 import Age from './Age';
-/*componente che ho creato prima che me lo va a mettere su Age*/
 
 export const Welcome = ({ name, age}) => {
   return (
     <div>
       <p>Welcome, {name} </p>
-      {(age > 18 && age < 65) && (name === "John" ) && <Age age={age} />}
+      {age > 18 ? ( <p>Your age is {age}</p>) : ( <p>You are very young!</p> )}
     </div>
   );
 }
