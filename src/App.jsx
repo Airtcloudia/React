2dc6ff5049032bb7ff2fdb513758417847f32180
Airtcloudia <klau26@hotmail.it>
2023-10-08 19:31:31 +0200

    add esercizio-36

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Clock from './Clock'
+import MouseClicker from './MouseClicker'
 
 
 function App() {
   
   return (
     <>
-      <Clock/>
+      <MouseClicker/>
     </>
   )
 }
---
+++ src/MouseClicker.jsx
@@ -0,0 +1,10 @@
+const MouseClicker = () => {
+    const handleButtonClick = (event) => {
+        console.log(event.target.name)
+    }
+    return (
+        <button name="one" onClick={handleButtonClick}>Click me</button>
+    )
+}
+
+export default MouseClicker
