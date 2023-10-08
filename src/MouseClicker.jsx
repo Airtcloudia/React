a7f3588eccc1f4db05d4068c56d2de9c53954257
Airtcloudia <klau26@hotmail.it>
2023-10-08 20:05:51 +0200

    add esercizio-39

--- src/MouseClicker.jsx
+++ src/MouseClicker.jsx
@@ -1,6 +1,6 @@
 const MouseClicker = () => {
     const handleButtonClick = (event) => {
-        console.log(event.currentTarget.name)
+        console.log(event.target.name)
     }
     return (
         <button name="one" onClick={handleButtonClick}> <img onClick={handleButtonClick} src ="" width={24} height={24}/>Click me</button>
@@ -9,5 +9,5 @@ const MouseClicker = () => {
 
 export default MouseClicker
 
-/*Il  name attributo del pulsante viene comunque stampato sulla console quando si fa clic sull'immagine perchè l'evento onClick è associato all'elemento su cui è stato definito
- (in questo caso, l'immagine), quindi quando viene cliccato l'immagine, verrà eseguito solo il codice associato a quell'evento ovvero handleButtonClick. */
+/*Com' è possibile impedire che il name attributo del pulsante venga stampato sulla console quando si fa clic sull'immagine? usando a riga 3 console.log(event.target.name)
+ in modo che prende solo l'onClick di button e non tutto quello all'interno del div*/
