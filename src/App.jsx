4a686862c1a410d8910cc38f5e650b2c05f1069e
Airtcloudia <klau26@hotmail.it>
2023-10-10 12:41:22 +0200

    add esercizio-42

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import InteractiveWelcome from './InteractiveWelcome'
+import Login from './Login'
+
 
 
 function App() {
   return (
-  <InteractiveWelcome/>
-     
+  <Login/>
   )
 }
 
--- src/Welcome.jsx
+++
@@ -1,6 +0,0 @@
-export const Welcome = (props)=> {
-const {name} = props;
-return (
-    <p>Welcome, {name}!</p>
-)
-}
