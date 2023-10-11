d0e38a3ac0495466c2993d74ebc2834293eecb3f
Airtcloudia <klau26@hotmail.it>
2023-10-11 16:01:28 +0200

    add esercizio-47

--- src/UncontrolledLogin.jsx
+++ src/UncontrolledLogin.jsx
@@ -1,6 +1,18 @@
 const UncontrolledLogin = () => {
+    const handleFormSubmit = (event) => {
+        event.preventDefault()
+        const formData = new FormData(event.target)
+
+        const data = {
+            username: formData.get("username"),
+            password: formData.get("password"),
+        }
+        console.log(data)
+        
+
+    }
     return (
-        <form>
+        <form onSubmit={handleFormSubmit}>
             <input name="username"/>
             <input name="password"  type="password"/>
             <input name="remember" type="checkbox"/>
