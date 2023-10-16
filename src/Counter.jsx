5b95aadeac60b0d837bcbb9d829fe4ebe8c63eb4
Airtcloudia <klau26@hotmail.it>
2023-10-16 19:29:59 +0200

    add esercizio-61

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,45 +1,49 @@
 import { useState, useRef, useEffect } from "react";
 import CounterDisplay from "./CounterDisplay";
 
-const Counter = ({initialValue, incrementAmount, decrementAmount}) => {
-    const [count, setCount] = useState(initialValue);
-    const prevCountRef = useRef(initialValue);
-    const ref = useRef("");
+const Counter = ({ initialValue, incrementAmount, decrementAmount }) => {
+  const [count, setCount] = useState(initialValue);
+  const prevCountRef = useRef(initialValue);
+  const ref = useRef("");
 
-    const handleCounterIncrement = () => {
-        setCount((count) => count + incrementAmount);
+  const handleCounterIncrement = () => {
+    setCount((count) => count + incrementAmount);
+  };
+
+  const handleCounterDecrement = () => {
+    setCount((count) => count - decrementAmount);
+  };
+
+  const handleCounterReset = () => {
+    setCount(initialValue);
+  };
+
+  useEffect(() => {
+    if (count > prevCountRef.current) {
+      ref.current = "up";
+    } else if (count < prevCountRef.current) {
+      ref.current = "down";
     }
 
-    const handleCounterDecrement = () => {
-        setCount((count) => count - decrementAmount);
+    if (ref.current !== prevCountRef.current) {
+      console.log(ref.current);
     }
 
-    const handleCounterReset = () => {
-        setCount(initialValue);
-    }
+    prevCountRef.current = count;
+  }, [count]);
 
-    useEffect(() => {
-        if (count > prevCountRef.current) {
-            ref.current = "up";
-        } else if (count < prevCountRef.current) {
-            ref.current = "down";
-        }
+  return (
+    <div style={{
+        backgroundColor:"violet",
+         //definisco lo stile in linea
 
-       if (ref.current !== prevCountRef.current) {
-            console.log(ref.current);
-        }
-
-        prevCountRef.current = count;
-    }, [count]);
-
-    return (
-        <>
-            <CounterDisplay content={count}/>
-            <button onClick={handleCounterIncrement}>Click me for increment!</button>
-            <button onClick={handleCounterDecrement}>Click me for decrement!</button>
-            <button onClick={handleCounterReset}>Click me for reset number!</button>
-        </>
-    )
-}
+    }}>
+      <CounterDisplay content={count} />
+      <button onClick={handleCounterIncrement}>Click me for increment!</button>
+      <button onClick={handleCounterDecrement}>Click me for decrement!</button>
+      <button onClick={handleCounterReset}>Click me for reset number!</button>
+    </div>
+  );
+};
 
 export default Counter;
