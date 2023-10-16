285cc3e3bcadb96ddaaa6dcbc4eaf0a857c3fc28
Airtcloudia <klau26@hotmail.it>
2023-10-16 19:35:38 +0200

    add esercizio-62

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,34 +1,40 @@
-import { useState } from "react"
+import React, { useState } from "react";
 
 const Login = () => {
     const [data, setData] = useState({
-        username: '',
-        password: '',
-        remember: false
-    }
-    )
+      username: '',
+      password: '',
+      remember: false
+    });
+  
     const handleInputChange = (event) => {
-const name = event.target.name
-const value = event.target.value 
-const checked = event.target.checked 
-const type = event.target.type
-
-setData((data) => {
-    return {
-        ...data,
-        [name] : type === 'checkbox' ? checked : value,
+      const name = event.target.name;
+      const value = event.target.value;
+      const checked = event.target.checked;
+      const type = event.target.type;
+  
+      setData((data) => {
+        return {
+          ...data,
+          [name]: type === 'checkbox' ? checked : value,
+        };
+      });
     }
-})
-    }
-
+  
+    const isPasswordValid = data.password.length >= 8;
+    const buttonStyle = {
+      backgroundColor: isPasswordValid ? "green" : "red",
+    };
+  
     return (
-        <>
-        <input name="username" value={data.username} onChange={handleInputChange} placeholder="username" />
-        <input name="password" value={data.password} onChange={handleInputChange} placeholder="password" />
-        <input name="session" type="checkbox" checked={data.session} onChange={handleInputChange} />
-        </>
-    )
-
-}
-
-export default Login
+      <>
+        <input
+          name="username" value={data.username} onChange={handleInputChange} placeholder="username"/>
+        <input name="password" value={data.password} onChange={handleInputChange} placeholder="password"/>
+        <input name="remember" type="checkbox" checked={data.remember} onChange={handleInputChange}/>
+        <button style={buttonStyle}>Login</button>
+      </>
+    );
+  };
+  
+  export default Login;
