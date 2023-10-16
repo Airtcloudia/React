cce669a75e33ac35b14f3e65af4802d8e62c23c1
Airtcloudia <klau26@hotmail.it>
2023-10-16 18:49:29 +0200

    add esercizio-54

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,13 @@ import './App.css'
 import Colors from './Colors'
 
 
-
 const App = () => {
  
-  return <Colors items={['red', 'green', 'blue']} />;
+  return <Colors items={[
+    { id: 1, name: 'Red' },
+    { id: 2, name: 'Green' },
+    { id: 3, name: 'Blue' }
+  ]} />;
 }
 
 export default App;
