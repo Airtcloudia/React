c95ca8d9397d3de5392b2bf90d5bc5dff54afa3b
Airtcloudia <klau26@hotmail.it>
2023-10-17 12:56:24 +0200

    add esercizio-67

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Login from './Login'
-
-
+import Container from './Container'
 
 function App() {
+
   return (
-  <Login/>
+    <>
+     <Container>
+      <h1>Welcome on Develhope community</h1>
+     </Container>
+    </>
   )
 }
 
---
+++ src/Container.jsx
@@ -0,0 +1,8 @@
+const Container = ({children}) => {
+    return (
+        <div style = {{backgroundColor: "white",
+         border: "1px solid: red" }}>{children}</div>
+    ) 
+}
+
+    export default Container
