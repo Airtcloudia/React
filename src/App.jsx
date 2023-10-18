7f3f56d85c97340afb1b149babcd7e19383c9521
Airtcloudia <klau26@hotmail.it>
2023-10-18 16:36:15 +0200

    add esercizio-37

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import MouseClicker from './MouseClicker'
-
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import MouseClicker from "./MouseClicker";
 
 function App() {
-  
   return (
     <>
-      <MouseClicker/>
+      <MouseClicker />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/MouseClicker.jsx
+++ src/MouseClicker.jsx
@@ -1,10 +1,14 @@
 const MouseClicker = () => {
-    const handleButtonClick = (event) => {
-        console.log(event.currentTarget.name)
-    }
-    return (
-        <button name="one" onClick={handleButtonClick}> <img width={24} height={24} />Click me</button>
-    )
-}
+  const handleButtonClick = (event) => {
+    console.log(event.currentTarget.name);
+  };
+  return (
+    <button name="one" onClick={handleButtonClick}>
+      {" "}
+      <img width={24} height={24} />
+      Click me
+    </button>
+  );
+};
 
-export default MouseClicker
+export default MouseClicker;
