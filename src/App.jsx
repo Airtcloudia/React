8cfe275781090e193d4a17a4a7748b253d3c410e
Airtcloudia <klau26@hotmail.it>
2023-10-18 13:51:12 +0200

    add esercizio-8

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import hello from "./Hello"
+import Hello from "./Hello"
 
 function App() {
   
   return (
-    <div>{hello()}</div>
+    <Hello/>
   )
 }
 
--- src/Hello.jsx
+++ src/Hello.jsx
@@ -1,12 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-const hello = () => {
+const Hello = () => {
     return (
     <h2>Hello, World!</h2>
     )
 }
 
-export default hello;
+export default Hello;
