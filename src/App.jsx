f8f34fb42addd2569dffae448a9bb837452a60c0
Airtcloudia <klau26@hotmail.it>
2023-10-18 16:22:12 +0200

    add esercizio-27

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Alertclock from './Alertclock'
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Alertclock from "./Alertclock";
 
 function App() {
-  
   return (
     <>
-      <Alertclock/>
+      <Alertclock />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
