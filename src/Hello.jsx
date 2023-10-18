d90978e0cef047b56cf7eb4f504992faf90af13d
Airtcloudia <klau26@hotmail.it>
2023-10-18 13:48:47 +0200

    add esercizio-7

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -1,12 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const Hello = () => {
+  return <h2>Hello, World!</h2>;
+};
 
-const hello = () => {
-    return (
-    <h2>Hello, World!</h2>
-    )
-}
-
-export default Hello
+export default Hello;
