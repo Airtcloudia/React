24ea0b3944b3cfeaa580cb57cfa3948a9f7375ef
Airtcloudia <klau26@hotmail.it>
2023-10-18 16:27:16 +0200

    add esercizio-30

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,20 +1,20 @@
 import { useState } from "react";
 
 const Counter = () => {
-    const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-    const handleIncrementCounter = () =>{
-        setCount((count) => count +1)
-    }
-    return (
-        <div>
-            <h2>Counter : {count}</h2>
-            <button onClick={handleIncrementCounter}>Increment</button>
-        </div>
-    )
-}
+  const handleIncrementCounter = () => {
+    setCount((count) => count + 1);
+  };
+  return (
+    <div>
+      <h2>Counter : {count}</h2>
+      <button onClick={handleIncrementCounter}>Increment</button>
+    </div>
+  );
+};
 
-export default Counter
+export default Counter;
 
 /*Quando si chiama la funzione "setter" per incrementare il contatore, il parametro deve essere una funzione o un valore immediato? Perché?
 Il parametro deve essere una funzione così da poter essere aggiornato in modo asyncrono.*/
