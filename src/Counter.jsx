eddcea207abb2494e5b001f546c6b6c4f8ed4c9e
Airtcloudia <klau26@hotmail.it>
2023-10-18 16:30:32 +0200

    add esercizio-32

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,20 +1,18 @@
 import { useState } from "react";
 import CounterDisplay from "./CounterDisplay";
 
+const Counter = ({ initialValue, incrementAmount }) => {
+  const [count, setCount] = useState(initialValue);
 
-const Counter = ({initialValue, incrementAmount}) => {
-    const [count, setCount] = useState(initialValue)
+  const handleIncrementCounter = () => {
+    setCount((count) => count + incrementAmount);
+  };
+  return (
+    <div>
+      <CounterDisplay content={count} />
+      <button onClick={handleIncrementCounter}>Increment</button>
+    </div>
+  );
+};
 
-    const handleIncrementCounter = () =>{
-        setCount((count) => count +incrementAmount)
-    }
-    return (
-        <div>
-            <CounterDisplay content={count}/>
-            <button onClick={handleIncrementCounter}>Increment</button>
-        </div>
-    )
-}
-
-export default Counter
-
+export default Counter;
