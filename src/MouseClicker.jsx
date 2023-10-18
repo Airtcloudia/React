d4f1ee94eedf0a2dff18158ef8125d61434960a1
Airtcloudia <klau26@hotmail.it>
2023-10-18 16:37:09 +0200

    add esercizio-38

--- src/MouseClicker.jsx
+++ src/MouseClicker.jsx
@@ -1,13 +1,17 @@
 const MouseClicker = () => {
-    const handleButtonClick = (event) => {
-        console.log(event.currentTarget.name)
-    }
-    return (
-        <button name="one" onClick={handleButtonClick}> <img onClick={handleButtonClick} src ="" width={24} height={24}/>Click me</button>
-    )
-}
+  const handleButtonClick = (event) => {
+    console.log(event.currentTarget.name);
+  };
+  return (
+    <button name="one" onClick={handleButtonClick}>
+      {" "}
+      <img onClick={handleButtonClick} src="" width={24} height={24} />
+      Click me
+    </button>
+  );
+};
 
-export default MouseClicker
+export default MouseClicker;
 
 /*Il  name attributo del pulsante viene comunque stampato sulla console quando si fa clic sull'immagine perchè l'evento onClick è associato all'elemento su cui è stato definito
  (in questo caso, l'immagine), quindi quando viene cliccato l'immagine, verrà eseguito solo il codice associato a quell'evento ovvero handleButtonClick. */
