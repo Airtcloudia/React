dba9a54e207183c1119ef802fb0872301fb397d0
Airtcloudia <klau26@hotmail.it>
2023-10-18 16:33:06 +0200

    add esercizio-34

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,31 +1,29 @@
 import { useEffect, useState } from "react";
 import CounterDisplay from "./CounterDisplay";
 
+const Counter = ({ initialValue, incrementAmount, decrementAmount }) => {
+  const [count, setCount] = useState(initialValue);
 
-const Counter = ({initialValue, incrementAmount, decrementAmount}) => {
-    const [count, setCount] = useState(initialValue)
+  const handleIncrementCounter = () => {
+    setCount((count) => count + incrementAmount);
+  };
+  const handleDecrementCounter = () => {
+    setCount((count) => count - decrementAmount);
+  };
+  const handleResetCounter = () => {
+    setCount(initialValue);
+  };
+  useEffect(() => {
+    console.log(`The value of the counter is ${count}`);
+  }, [count]);
+  return (
+    <div>
+      <CounterDisplay content={count} />
+      <button onClick={handleIncrementCounter}>Increment</button>
+      <button onClick={handleDecrementCounter}>Decrement</button>
+      <button onClick={handleResetCounter}>Reset</button>
+    </div>
+  );
+};
 
-    const handleIncrementCounter = () =>{
-        setCount((count) => count +incrementAmount)
-    }
-    const handleDecrementCounter = () => {
-        setCount((count) => count -decrementAmount)
-    }
-    const handleResetCounter = () => {
-        setCount(initialValue)
-    }
-    useEffect (() => {
-      console.log(`The value of the counter is ${count}`)
-    }, [count])
-    return (
-        <div>
-            <CounterDisplay content={count}/>
-            <button onClick={handleIncrementCounter}>Increment</button>
-            <button onClick={handleDecrementCounter}>Decrement</button>
-            <button onClick={handleResetCounter}>Reset</button>
-        </div>
-    )
-}
-
-export default Counter
-
+export default Counter;
