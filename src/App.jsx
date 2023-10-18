163c2778cfb01a819f2dcc16201d74d3e3b4e336
Airtcloudia <klau26@hotmail.it>
2023-10-18 16:34:02 +0200

    add esercizio-35

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Clock from './Clock'
-
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Clock from "./Clock";
 
 function App() {
-  
   return (
     <>
-      <Clock/>
+      <Clock />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Clock.jsx
+++ src/Clock.jsx
@@ -1,18 +1,18 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 const Clock = () => {
-    const [date, setdate ] = useState(new Date())
+  const [date, setdate] = useState(new Date());
 
-    useEffect(() => {
-        setInterval(() => {
-            setdate(new Date())
-        }, 1000)
-    }, [])
- return (
+  useEffect(() => {
+    setInterval(() => {
+      setdate(new Date());
+    }, 1000);
+  }, []);
+  return (
     <div>
-        <h2>The time now is: {date.toLocaleTimeString()}</h2>
+      <h2>The time now is: {date.toLocaleTimeString()}</h2>
     </div>
- )
-}
+  );
+};
 
-export default Clock
+export default Clock;
