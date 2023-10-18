8594f719fa1b87c5ddaf40715749d35afc969f7b
Airtcloudia <klau26@hotmail.it>
2023-10-18 14:21:35 +0200

    esercizio-18

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Welcome } from './Welcome'
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Welcome } from "./Welcome";
 
 function App() {
-  return (
-   <Welcome name = "Claudia" age="25"/>
-     
-  )
+  return <Welcome name="Claudia" age="25" />;
 }
 
-export default App
+export default App;
