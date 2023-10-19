9b84871521fe6d81e4b69c5523ec338b3ab433fe
Airtcloudia <klau26@hotmail.it>
2023-10-19 22:40:05 +0200

    add esercizio-73

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,11 @@
 import { useState, useContext } from "react";
 import "./App.css";
-import Clock from "./Clock";
-import LanguageContext from "./LanguageContext";
+import GitHubUser from "./GitHubUser";
 
 function App() {
-  const [language, setLanguage] = useState("it");
-
-  const handleSetLanguage = (language) => {
-    setLanguage(language);
-  };
-
-  return (
-    <div>
-      <button onClick={() => handleSetLanguage("it")}>IT</button>
-      <button onClick={() => handleSetLanguage("en")}>EN</button>
-      {/* <LanguageContext.Provider value={language}> /}
-        <Clock />
-      {/ </LanguageContext.Provider> */}
-    </div>
-  );
+  return <GitHubUser username={"Airtcloudia"} />;
 }
 
 export default App;
-
-// Rimuovere il provider di contesto. Cosa succede al Clockcomponente? Perché?
-//Rimuovendo "LanguageContext.provider" il componente Clock non ha dunque, più accesso al componente LanguageContext e quindi il parametro language risulterà undefined;
-// l'orario verrà comunque mostrato aggiornato ma non sarà possibile fare lo switch tra i due linguaggi (IT,EN).
 
 
