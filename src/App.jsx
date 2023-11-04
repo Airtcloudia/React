4a93f744130e5746937cb9d1363ab629b26cb527
Airtcloudia <klau26@hotmail.it>
2023-11-04 16:11:48 +0100

    add esercizio-86

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,21 @@ import {Routes, Route, Link} from "react-router-dom"
 import ShowGithubUser from "./ShowGithubUser";
 import { Welcome } from "./Welcome";
 import Counter from "./Counter";
+import NotFound from "./NotFound";
 
-const App = () => {
+function App() {
   return (
     <>
-    <nav>
+    <nav className="flex-row gap-10" >
       <Link to="/" element={<Welcome/>}>Home</Link>
       <Link to="/users/:username" element={<ShowGithubUser/>}>ShowGithubUser</Link>
       <Link to="/counter" element={<Counter/>}>Counter</Link>
     </nav>
     <Routes>
-       <Route exact path="/" element={<Welcome name="Airtcloudia"/>} />
+       <Route path="*" element={<NotFound />} />
+       <Route exact path="/" element={<Welcome name="Claudia"/>} />
        <Route path="/users/:username" element={<ShowGithubUser/>}/>
-       <Route path="/counter" element={<Counter initialValue={0} incrementAmount={1} decrementAmount={1} />}/>
+       <Route path="/counter" element={<Counter/>}/>
     </Routes>
     </>
   );
