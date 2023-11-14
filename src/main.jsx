f5cc22a3b5d79aab502ecb310af80a03a02b0f4b
Airtcloudia <klau26@hotmail.it>
2023-11-14 13:01:12 +0100

    add esercizio-92

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { SWRConfig } from 'swr';
+import {BrowserRouter} from "react-router-dom"
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-)
+const fetcher = url => fetch(url).then(res => res.json());
+
+const root = document.getElementById('root');
+
+const rootElement = ReactDOM.createRoot(root);
+
+rootElement.render(
+    <SWRConfig value={{ fetcher }}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </SWRConfig>
+);
